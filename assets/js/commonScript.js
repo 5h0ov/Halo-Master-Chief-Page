a790ac46af0eaf09bf3063eb5557e6f944923e90
shoob <shuvadiptadas8820@gmail.com>
2023-08-11 10:49:20 +0530

    Added Scroll To Top Button

--- assets/js/commonScript.js
+++ assets/js/commonScript.js
@@ -10,3 +10,23 @@ menBtn.addEventListener('click', () =>{
     menu.classList.toggle('menu-open');
 });
 
+
+// Get the button
+let mybutton = document.getElementById("myBtn");
+    
+// When the user scrolls down 1000px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
--- script.js
+++ script.js
@@ -23,3 +23,22 @@ window.addEventListener("scroll", () =>{
     }
 });
 
+// Get the button
+let mybutton = document.getElementById("myBtn");
+    
+// When the user scrolls down 1000px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
